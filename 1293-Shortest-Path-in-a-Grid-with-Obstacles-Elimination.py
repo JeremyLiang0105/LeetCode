4a6644247aac8c8539b496c6ce12ee1f9d343c6b
JeremyLiang0105 <66391583+JeremyLiang0105@users.noreply.github.com>
2022-03-19 21:22:17 -0400

    Create 1293-Shortest-Path-in-a-Grid-with-Obstacles-Elimination.py

---
+++ 1293-Shortest-Path-in-a-Grid-with-Obstacles-Elimination.py
@@ -0,0 +1,26 @@
+class Solution:
+    def shortestPath(self, grid: List[List[int]], k: int) -> int:
+        m, n = len(grid), len(grid[0])
+        visited = set()
+        res = -1
+        
+        q = deque()
+        q.append([0, 0, k, 0])
+        
+        while(q):
+            r, c, left, steps = q.popleft()
+            if r == m - 1 and c == n - 1:
+                res = steps
+                return res
+            
+            if grid[r][c] == 1:
+                if left <= 0:
+                    continue
+                left -= 1
+            lst = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+            for x, y in lst:
+                newR, newC = r + x, c + y
+                if 0 <= newR < m and 0 <= newC < n and (newR, newC, left) not in visited:
+                    q.append([newR, newC, left, steps + 1])
+                    visited.add((newR, newC, left))
+        return res
