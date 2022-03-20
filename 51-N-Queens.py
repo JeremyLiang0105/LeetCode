73c4ef4ec81a7dcde315f1ff4b1e77c3d594202c
JeremyLiang0105 <66391583+JeremyLiang0105@users.noreply.github.com>
2022-03-20 14:46:23 -0400

    Create 51-N-Queens.py

---
+++ 51-N-Queens.py
@@ -0,0 +1,31 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        col = set()
+        posDiag = set() #(r + c)
+        negDiag = set() #(r - c)
+        
+        res = []  
+        board = [["."] * n for i in range(n)] # create the n*n board 
+        # board = [["." for j in range(n)] for i in range(n)]
+        
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(r) for r in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                col.add(c)
+                posDiag.add(r + c)
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r + 1)
+                col.remove(c)
+                posDiag.remove(r + c)
+                negDiag.remove(r - c)
+                board[r][c] = "."
+        backtrack(0)
+        return res
