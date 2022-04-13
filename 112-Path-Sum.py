6b89c27dde627cb1d9741a2a1a7b74df914ee0b7
JeremyLiang0105 <66391583+JeremyLiang0105@users.noreply.github.com>
2022-04-12 23:49:06 -0400

    Create 112-Path-Sum.py

---
+++ 112-Path-Sum.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: TreeNode, targetSum: int) -> bool:
+        def dfs(node, curSum):
+            if not node:
+                return False
+            curSum += node.val
+            if node.left == None and node.right == None:
+                return curSum == targetSum
+            return dfs(node.left, curSum) or dfs(node.right, curSum)
+        return dfs(root, 0)
