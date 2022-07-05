9de326f08c44d5b2e29191ea3e876962a7c69923
JeremyLiang0105 <66391583+JeremyLiang0105@users.noreply.github.com>
2022-07-04 21:19:44 -0400

    Create 652-Find-Duplicate-Subtrees.py

---
+++ 652-Find-Duplicate-Subtrees.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findDuplicateSubtrees(self, root: Optional[TreeNode]) -> List[Optional[TreeNode]]:
+        res = []
+        lookup = {}
+        
+        def dfs(node):
+            if not node:
+                return "N"
+            l = dfs(node.left)
+            r = dfs(node.right)
+            path = str(node.val) + "," + l + "," + r
+            lookup[path] = 1 + lookup.get(path, 0)
+            if lookup[path] == 2:
+                res.append(node)
+            return path
+        dfs(root)
+        print(lookup)
+    
+    # time complexity: O(n^2)
