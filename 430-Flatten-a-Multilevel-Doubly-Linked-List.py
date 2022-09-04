99c6fff5ea2e35293af0ab8253f46c5cfb8fe115
JeremyLiang0105 <66391583+JeremyLiang0105@users.noreply.github.com>
2022-09-04 16:50:47 -0400

    Create 430-Flatten-a-Multilevel-Doubly-Linked-List.py

---
+++ 430-Flatten-a-Multilevel-Doubly-Linked-List.py
@@ -0,0 +1,29 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+"""
+
+class Solution:
+    def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        cur = head
+        stack = []
+        
+        while cur:
+            if cur.child:
+                if cur.next:
+                    stack.append(cur.next)
+                cur.next = cur.child
+                cur.next.prev = cur
+                cur.child = None
+            
+            while cur.next == None and len(stack) != 0:
+                cur.next = stack.pop()
+                cur.next.prev = cur
+            
+            cur = cur.next
+        return head
